5eada80b324dd4cb85e5293e7a906f01a6f5ff6f
BrunoFNRodrigues <17669@gmail.com>
2020-03-25 14:24:24 -0300

    última atulização de todos os asquivos

--- Jogo.py
+++ Jogo.py
@@ -2,8 +2,17 @@
 print('Bem vindo(a) ao Kassinão!')
 print('Já que é a sua primeira vez toma aí 1000 fichas')
 print('Bora jogar Craps')
-print('Agora estamos no Come Out. Você pode fazer as seguintes apostas: Pass Line Bet, Field, Any Craps')
-tipo=input('Qual vai ser o seu tipo de aposta:')
-aposta=int(input('Quantas fichas você vai apostar:'))
-ficha=1000
+resposta=input('Você vai querer jogar?s/n')
+if resposta=='s':
+    print('Agora estamos no Come Out. Você pode fazer as seguintes apostas: Pass Line Bet, Field, Any Craps')
+    tipo=input('Qual vai ser o seu tipo de aposta:')
+    aposta=int(input('Quantas fichas você vai apostar:'))
+    ficha=1000
+else:
+    print('Até a proxima!')
 
+Pass_Line=False
+Field=False
+Any_Craps=False
+Twelve=False
+if tipo==""
