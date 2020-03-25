df91b8066774add42abadd936ac7179f89249cec
BrunoFNRodrigues <17669@gmail.com>
2020-03-25 11:18:45 -0300

    Introdução do jogo

---
+++ Jogo.py
@@ -0,0 +1,7 @@
+#Inicio do DO Jogo
+print('Bem vindo(a) ao Kassinão!')
+print('Já que é a sua primeira vez toma aí 1000 fichas')
+print('Bora jogar Craps')
+tipo=input('Qual vai ser o seu tipo de aposta:')
+aposta=int(input('Quantas fichas você vai apostar:'))
+ficha=1000
