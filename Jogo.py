078bfd8e7b3281e6e2ada494b993c6de83ffca89
BrunoFNRodrigues <17669@gmail.com>
2020-03-29 14:11:29 -0300

    Versão final 1 da introdução

--- Jogo.py
+++ Jogo.py
@@ -2,17 +2,27 @@
 print('Bem vindo(a) ao Kassinão!')
 print('Já que é a sua primeira vez toma aí 1000 fichas')
 print('Bora jogar Craps')
-resposta=input('Você vai querer jogar?s/n')
-if resposta=='s':
-    print('Agora estamos no Come Out. Você pode fazer as seguintes apostas: Pass Line Bet, Field, Any Craps')
-    tipo=input('Qual vai ser o seu tipo de aposta:')
-    aposta=int(input('Quantas fichas você vai apostar:'))
-    ficha=1000
-else:
-    print('Até a proxima!')
 
+#Variaveis
+ficha=1000
 Pass_Line=False
 Field=False
 Any_Craps=False
 Twelve=False
+
+#Resposta do user
+resposta=input('Você vai querer jogar?(s/n)')
+if resposta=='s':
+    print('Agora estamos no Come Out. Você pode fazer as seguintes apostas: Pass Line Bet, Field, Any Craps, Twelve.')
+    tipo=input('Qual vai ser o tipo da aposta:')
+    aposta=int(input('Quantas fichas você vai apostar:'))
+    resposta=input('Quer fazer mais alguma aposta?(s/n)')
+    while resposta=='s':
+        tipo=input('Qual vai ser o tipo da aposta:')
+        aposta=int(input('Quantas fichas você vai apostar:'))
+        tipo=True
+        resposta=input('Quer fazer mais alguma aposta?(s/n)')
+else:
+    print('Até a proxima!')
+
 if tipo==""
