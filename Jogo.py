65d8a9b57683b4bcb857468c6eb8a82aa6c79efe
BrunoFNRodrigues <17669@gmail.com>
2020-03-29 15:03:58 -0300

    Faze de pass line e point

--- Jogo.py
+++ Jogo.py
@@ -9,6 +9,8 @@ Pass_Line=False
 Field=False
 Any_Craps=False
 Twelve=False
+#Roladoe de dados
+dados=0
 
 #Resposta do user
 resposta=input('Você vai querer jogar?(s/n)')
@@ -16,13 +18,47 @@ if resposta=='s':
     print('Agora estamos no Come Out. Você pode fazer as seguintes apostas: Pass Line Bet, Field, Any Craps, Twelve.')
     tipo=input('Qual vai ser o tipo da aposta:')
     aposta=int(input('Quantas fichas você vai apostar:'))
+    ficha-=aposta
     resposta=input('Quer fazer mais alguma aposta?(s/n)')
     while resposta=='s':
         tipo=input('Qual vai ser o tipo da aposta:')
         aposta=int(input('Quantas fichas você vai apostar:'))
+        ficha-=aposta
         tipo=True
         resposta=input('Quer fazer mais alguma aposta?(s/n)')
 else:
     print('Até a proxima!')
 
-if tipo==""
+#Aposta pass line bet
+if Pass_Line:
+    Point=False
+    if dados == 7 or 11:
+        ficha+=aposta*2
+        print('Parabéns você ganhou, agora você tem {} fichas.'.format(ficha))
+        Pass_Line=False
+    elif dados == 2 or 3 or 12:
+        print('Infelizmente você perdeu, agora você tem {} fichas'.format(ficha))
+        Pass_Line=False
+    else:
+        print('Saiu {0} nos dados agora estamos na faze de Point')
+        Pass_Line=False
+        Point=True
+        #Faze de Point
+        if Point:
+            print('Agora estamos no Point. Você pode fazer as seguintes apostas: Field, Any Craps, Twelve.')
+            resposta=input('Quer fazer mais alguma aposta?(s/n)')
+            while resposta=='s':
+                tipo=input('Qual vai ser o tipo da aposta:')
+                aposta=int(input('Quantas fichas você vai apostar:'))
+                ficha-=aposta
+                tipo=True
+                resposta=input('Quer fazer mais alguma aposta?(s/n)')
+            ponto=dados
+            dados=10
+            i=0
+            while dados!=ponto:
+                dados=0
+                
+                i+=1
+
+
